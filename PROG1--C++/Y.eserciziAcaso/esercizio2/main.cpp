b933f2c92c52364f38e2c9087ef729afc41099a8
VOMA <andrea@portesi.com>
2025-10-08 16:33:37 +0200

    Add drawHeart function and integrate it into main program

--- PROG1--C++/Y.eserciziAcaso/esercizio2/main.cpp
+++ PROG1--C++/Y.eserciziAcaso/esercizio2/main.cpp
@@ -15,11 +15,35 @@ void drawCircle(int radius) {
         cout << endl;
     }
 }
-
+void drawHeart(int size) {
+    for (int y = size / 2; y <= size; y += 2) {
+        for (int x = 1; x < size - y; x += 2)
+            cout << " ";
+        for (int x = 1; x <= y; x++)
+            cout << "*";
+        for (int x = 1; x <= size - y; x++)
+            cout << " ";
+        for (int x = 1; x <= y; x++)
+            cout << "*";
+        cout << endl;
+    }
+    for (int y = size; y >= 1; y--) {
+        for (int x = 0; x < size - y; x++)
+            cout << " ";
+        for (int x = 1; x <= y * 2 - 1; x++)
+            cout << "*";
+        cout << endl;
+    }
+}
 int main() {
     int r;
+    int size;
     cout << "Inserisci il raggio del cerchio: ";
     cin >> r;
+    cout << "inserisci grandezza cuore: ";
+    cin >> size;
     drawCircle(r);
+    cout << endl;
+    drawHeart(size);
     return 0;
 }
