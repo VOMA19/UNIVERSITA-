d775554bf609ae38eb228f4add22c2daa88d3148
VOMA <andrea@portesi.com>
2025-10-11 15:00:44 +0200

    Refactor controllo function to use stack for balanced parentheses validation

--- PROG1--C++/N.eserciziNest/esercizio6/main.cpp
+++ PROG1--C++/N.eserciziNest/esercizio6/main.cpp
@@ -1,60 +1,35 @@
-
 #include <iostream>
 using namespace std;
-//parentesizazzione perfetta in un array controllare se i caratteri parentesi sono corretti () --> ok (() --> non ok
-bool controllo(char parentesi[]);
-int main(){
 
-    char parentesi[] = {'(','(',')',')',')','(','\0'};
-    bool corr = controllo(parentesi);
+bool controllo(const char parentesi[]) {
+    char stack[100];
+    int top = -1;
 
-    if(corr)
-        cout << "la parentesizzazione è perfetta" <<endl;
-    else
-        cout << "la parentesizzazione non è perfetta" <<endl;
-
-    return 0;
-}
-
-bool controllo(char parentesi[]){
-    int cont = 0;
-    int contGraffe = 0;
-    int contQuad = 0;
-
-    for (int i = 0; parentesi[i] != '\0'; i++){
-
-        if (parentesi[i] == '(') {
-            cont++;  
-
-        }else if(parentesi[i] == ')'){
-            cont--;  
-            if (cont < 0) 
+    for (int i = 0; parentesi[i] != '\0'; i++) {
+        char c = parentesi[i];
+        if (c == '(' || c == '[' || c == '{') {
+            stack[++top] = c;
+        } else if (c == ')' || c == ']' || c == '}') {
+            if (top < 0)
                 return false;
-
-        }else if(parentesi[i] == '{'){
-            contGraffe ++;
-        }else if (parentesi[i] == '}'){
-            
-            contGraffe --;
-            if (contGraffe < 0) {
-                
+            char open = stack[top--];
+            if ((c == ')' && open != '(') ||
+                (c == ']' && open != '[') ||
+                (c == '}' && open != '{'))
                 return false;
-            }
-        }else if(parentesi[i] == '['){
-            contQuad ++;
-        }else if (parentesi[i] == ']'){
-            contQuad --;
-            if (contQuad < 0) {
-                
-                return false;
-            }
         }
     }
+    return top == -1;
+}
 
-    if(cont == 0 && contGraffe == 0 && contQuad == 0){
-        return true;
-    }else {
-        return false;
-    }
+int main() {
+    char parentesi[] = {'[','(',']',')','\0'};
+    bool corr = controllo(parentesi);
 
+    if (corr)
+        cout << "la parentesizzazione è perfetta" << endl;
+    else
+        cout << "la parentesizzazione non è perfetta" << endl;
+
+    return 0;
 }
