1209424487579410a2883437f2d89f645da2733b
VOMA <andrea@portesi.com>
2025-10-16 23:02:46 +0200

    Refactor main function to include number input, array reversal, and output of reversed array

--- PROG1--C++/N.eserciziNest/esercizio2.4/main.cpp
+++ PROG1--C++/N.eserciziNest/esercizio2.4/main.cpp
@@ -5,7 +5,25 @@ const int N = 10;
 int main(){
     int numeri[N];
 
-    
+    // Leggi i numeri
+    for (int i = 0; i < N; i++) {
+        cout << "Inserisci numero " << i + 1 << ": ";
+        cin >> numeri[i];
+    }
+
+    // Reverse dell'array
+    for (int i = 0; i < N / 2; i++) {
+        int temp = numeri[i];
+        numeri[i] = numeri[N - 1 - i];
+        numeri[N - 1 - i] = temp;
+    }
+
+    // Stampa array invertito
+    cout << "\nArray invertito: ";
+    for (int i = 0; i < N; i++) {
+        cout << numeri[i] << " ";
+    }
+    cout << endl;
 
     return 0;
 }
