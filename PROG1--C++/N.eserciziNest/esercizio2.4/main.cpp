90b3c7a2c864ca2ad99cd2b5010faa8ea6de9789
VOMA <andrea@portesi.com>
2025-11-02 18:58:39 +0100

    nulla di nuovo, sistemazione di alcune cartelle

--- PROG1--C++/N.eserciziNest/esercizio2.4/main.cpp
+++ PROG1--C++/N.eserciziNest/esercizio2.4/main.cpp
@@ -2,28 +2,42 @@
 using namespace std;
 const int N = 10;
 
+void fillArray(int numeri[]);
+void reverseArray(int numeri[]);
+void printRArray( int numeri[]);
+
 int main(){
     int numeri[N];
 
-    // Leggi i numeri
+    fillArray(numeri);
+    reverseArray(numeri);
+    printRArray(numeri);
+    cout << endl;
+
+    return 0;
+}
+
+// Leggi i numeri
+void fillArray(int numeri[]){
     for (int i = 0; i < N; i++) {
         cout << "Inserisci numero " << i + 1 << ": ";
         cin >> numeri[i];
     }
+}
 
-    // Reverse dell'array
+// Reverse dell'array
+void reverseArray(int numeri[]){
     for (int i = 0; i < N / 2; i++) {
         int temp = numeri[i];
         numeri[i] = numeri[N - 1 - i];
         numeri[N - 1 - i] = temp;
     }
+}
 
-    // Stampa array invertito
+// Stampa array invertito
+void printRArray( int numeri[]){
     cout << "\nArray invertito: ";
     for (int i = 0; i < N; i++) {
         cout << numeri[i] << " ";
     }
-    cout << endl;
-
-    return 0;
 }
