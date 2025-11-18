9c040789fd2d3b447aa4e58ab780258595aa3ff2
VOMA <andrea@portesi.com>
2025-11-18 23:50:44 +0100

    Add C++ matrix spiral filling and column sum functionality with recursive methods

--- PROG1--C++/N.eserciziNest/esercizio5.4Rec/main.cpp
+++ PROG1--C++/N.eserciziNest/esercizio5.4Rec/main.cpp
@@ -1,84 +1,131 @@
 #include <iostream>
 using namespace std;
 
-#define N 10
+#define N 7
 
-void fillMatrixSpiral(int mat[N][N]);
-void fillMatrixSpiralRec(int mat[N][N], int top, int bottom, int left, int right, int &num);
-int sumColumn(int mat[N][N], int col);
-int sumColumnRec(int mat[N][N], int col, int row);
-void printMatrix(int mat[N][N]);
-void printMatrixRec(int mat[N][N], int row, int col);
+void riempiMatriceSpiral(int mat[N][N]); 
+void riempiMatriceSpiraleRic(int mat[N][N], int alto, int basso, int sinistra, int destra, int &num);  
+void riempiDestraRic(int mat[N][N], int riga, int col, int destra, int &num);  
+void riempiGiuRic(int mat[N][N], int riga, int basso, int col, int &num);  
+void riempiSinistraRic(int mat[N][N], int riga, int sinistra, int col, int &num);   
+void riempiSuRic(int mat[N][N], int riga, int alto, int col, int &num);      
+int sommaColonna(int mat[N][N], int col); 
+int sommaColonnaRic(int mat[N][N], int col, int riga);  
+void stampaMatrice(int mat[N][N]);  
+void stampaMatriceRic(int mat[N][N], int riga, int col);  
 
-int main(){
+int main() {
     int mat[N][N];
-    fillMatrixSpiral(mat);
-    printMatrix(mat);
+    riempiMatriceSpiral(mat);  
+    stampaMatrice(mat);       
 
     int column;
     cout << "Inserisci il numero della colonna (0-" << N-1 << "): ";
     cin >> column;
     
     if (column >= 0 && column < N) {
-        cout << "Somma colonna " << column << ": " << sumColumn(mat, column) << endl;
+        cout << "Somma colonna " << column << ": " << sommaColonna(mat, column) << endl;
     }
     
     return 0;
 }
 
-void fillMatrixSpiral(int mat[N][N]) {
-    int num = 1;
-    fillMatrixSpiralRec(mat, 0, N-1, 0, N-1, num);
+// Funzione wrapper per iniziare il riempimento spirale
+void riempiMatriceSpiral(int mat[N][N]) {
+    int num = 1;  // Contatore che inizia da 1
+    riempiMatriceSpiraleRic(mat, 0, N-1, 0, N-1, num);
 }
 
-void fillMatrixSpiralRec(int mat[N][N], int top, int bottom, int left, int right, int &num) {
-    if (top > bottom || left > right) return;
+// Riempie ricorsivamente da sinistra a destra
+void riempiDestraRic(int mat[N][N], int riga, int col, int destra, int &num) {
+    if (col > destra) 
+        return;  // Caso base: superato il limite destro
     
-    // Destra
-    for (int i = left; i <= right; i++)
-        mat[top][i] = num++;
+    mat[riga][col] = num++;    // Assegna il numero corrente e incrementa
+    riempiDestraRic(mat, riga, col + 1, destra, num);  // Ricorsione sulla colonna successiva
+}
+
+// Riempie ricorsivamente dall'alto in basso
+void riempiGiuRic(int mat[N][N], int riga, int basso, int col, int &num) {
+    if (riga > basso) 
+        return;  // Caso base: superato il limite inferiore
     
-    // Basso
-    for (int i = top+1; i <= bottom; i++)
-        mat[i][right] = num++;
+    mat[riga][col] = num++;     // Assegna il numero corrente e incrementa
+    riempiGiuRic(mat, riga + 1, basso, col, num);  // Ricorsione sulla riga successiva
+}
+
+// Riempie ricorsivamente da destra a sinistra
+void riempiSinistraRic(int mat[N][N], int riga, int sinistra, int col, int &num) {
+    if (col < sinistra) 
+        return;   // Caso base: superato il limite sinistro
     
-    // Sinistra
-    if (top < bottom) {
-        for (int i = right-1; i >= left; i--)
-            mat[bottom][i] = num++;
+    mat[riga][col] = num++;    // Assegna il numero corrente e incrementa
+    riempiSinistraRic(mat, riga, sinistra, col - 1, num);  // Ricorsione sulla colonna precedente
+}
+
+// Riempie ricorsivamente dal basso verso l'alto
+void riempiSuRic(int mat[N][N], int riga, int alto, int col, int &num) {
+    if (riga < alto) 
+        return;    // Caso base: superato il limite superiore
+    
+    mat[riga][col] = num++;    // Assegna il numero corrente e incrementa
+    riempiSuRic(mat, riga - 1, alto, col, num);  // Ricorsione sulla riga precedente
+}
+
+// Funzione ricorsiva principale per il riempimento a spirale
+void riempiMatriceSpiraleRic(int mat[N][N], int alto, int basso, int sinistra, int destra, int &num) {
+    if (alto > basso || sinistra > destra) 
+        return;  // Caso base: limiti incrociati
+    
+    // Riempie il lato superiore (da sinistra a destra)
+    riempiDestraRic(mat, alto, sinistra, destra, num);
+    
+    // Riempie il lato destro (dall'alto in basso, saltando l'angolo già riempito)
+    riempiGiuRic(mat, alto + 1, basso, destra, num);
+    
+    // Riempie il lato inferiore (da destra a sinistra) solo se c'è più di una riga
+    if (alto < basso) {
+        riempiSinistraRic(mat, basso, sinistra, destra - 1, num);
     }
     
-    // Alto
-    if (left < right) {
-        for (int i = bottom-1; i >= top+1; i--)
-            mat[i][left] = num++;
+    // Riempie il lato sinistro (dal basso verso l'alto) solo se c'è più di una colonna
+    if (sinistra < destra) {
+        riempiSuRic(mat, basso - 1, alto + 1, sinistra, num);
     }
     
-    fillMatrixSpiralRec(mat, top+1, bottom-1, left+1, right-1, num);
+    // Ricorsione per lo strato interno della spirale
+    riempiMatriceSpiraleRic(mat, alto+1, basso-1, sinistra+1, destra-1, num);
 }
 
-int sumColumn(int mat[N][N], int col) {
-    return sumColumnRec(mat, col, 0);
+// Funzione wrapper per calcolare la somma di una colonna
+int sommaColonna(int mat[N][N], int col) {
+    return sommaColonnaRic(mat, col, 0);  // Inizia dalla riga 0
 }
 
-int sumColumnRec(int mat[N][N], int col, int row) {
-    if (row >= N) return 0;
-    return mat[row][col] + sumColumnRec(mat, col, row + 1);
-}
-
-void printMatrix(int mat[N][N]) {
-    printMatrixRec(mat, 0, 0);
-}
-
-void printMatrixRec(int mat[N][N], int row, int col) {
-    if (row >= N) return;
+// Calcola ricorsivamente la somma degli elementi di una colonna
+int sommaColonnaRic(int mat[N][N], int col, int riga) {
+    if (riga >= N) 
+        return 0;  // Caso base: fine della colonna
     
-    cout << mat[row][col] << "\t";
+    return mat[riga][col] + sommaColonnaRic(mat, col, riga + 1);  // Somma elemento corrente + ricorsione
+}
+
+// Funzione wrapper per stampare la matrice
+void stampaMatrice(int mat[N][N]) {
+    stampaMatriceRic(mat, 0, 0);  // Inizia dalla posizione (0,0)
+}
+
+// Stampa ricorsivamente la matrice
+void stampaMatriceRic(int mat[N][N], int riga, int col) {
+    if (riga >= N) 
+        return;  // Caso base: fine della matrice
     
-    if (col == N-1) {
+    cout << mat[riga][col] << "\t";  // Stampa l'elemento corrente
+    
+    if (col == N-1) {  // Fine della riga
         cout << endl;
-        printMatrixRec(mat, row + 1, 0);
+        stampaMatriceRic(mat, riga + 1, 0);  // Vai alla riga successiva
     } else {
-        printMatrixRec(mat, row, col + 1);
+        stampaMatriceRic(mat, riga, col + 1);  // Vai alla colonna successiva
     }
 }
